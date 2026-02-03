466d501e49a9dbc5684ff415e07d49842ec0b857
JunzhouMou <mouj@uwindsor.ca>
2026-02-02 20:16:20 -0500

    removed unnecessary files.

--- main.py
+++ main.py
@@ -10,18 +10,18 @@ from hypergraph.hypergraph import graph_to_hypergraph
 
 
 if __name__ == "__main__":
-    edges = read_edges_from_file('data/edges-karate.txt')
+    edges = read_edges_from_file("data/edges-karate.txt")
     np.random.seed(42)
     # Create the graph
     G = nx.Graph()
     G.add_edges_from(edges)
 
     for node in G.nodes():
-      degree = G.degree(node)
-      clustering_coeff = nx.clustering(G, node)
-      # You can also add more complex features or external data
-      random_feature = np.random.random()  # Example of adding random features
-      G.nodes[node]['features'] = np.array([degree, clustering_coeff, random_feature])
+        degree = G.degree(node)
+        clustering_coeff = nx.clustering(G, node)
+        # You can also add more complex features or external data
+        random_feature = np.random.random()  # Example of adding random features
+        G.nodes[node]["features"] = np.array([degree, clustering_coeff, random_feature])
 
     # Parameters
     k = 3
@@ -31,8 +31,10 @@ if __name__ == "__main__":
     k_hop = 1
 
     # Find the top-k overlapping densest subgraphs
-    subgraphs = top_k_overlapping_densest_subgraphs(G, k, lambda_param, min_subset_size, max_subset_size, k_hop)
-    
+    subgraphs = top_k_overlapping_densest_subgraphs(
+        G, k, lambda_param, min_subset_size, max_subset_size, k_hop
+    )
+
     # Instantiating a dictionary for assing nodes to each subgraph
     hyper_dic = {}
 
@@ -40,23 +42,35 @@ if __name__ == "__main__":
     for i, sg in enumerate(subgraphs, 1):
         print(f"Subgraph {i}: Nodes = {sg.nodes()}, Edges = {sg.edges()}")
         hyper_dic[f"Subgraph {i}"] = set(sg.nodes())
-    
-    
+
     hypergraph = graph_to_hypergraph(hyper_dic)
     print(f"Hypergraph whcih is created with hyperedges of ---> {hypergraph.e}")
-    
+
     # Make directory for saving the output
     path = outputdir(f"Karate_club_K={k}_lambda={lambda_param}")
-    
+
     # Save subgraphs into a JSON file
-    subgraphs_json = savesubgraphs(hyper_dic, path) 
-    
+    subgraphs_json = savesubgraphs(hyper_dic, path)
+
     # Load subgraphs if it is necessary from JSON file
-#     hyper_dic = readsubgraphs(path)
-    
+    #     hyper_dic = readsubgraphs(path)
+
     # Plot the original graph and subgraphs
-    plot_save_graph(G, k, lambda_param, min_subset_size, max_subset_size, k_hop, path, title="Original Graph")
-    plot_save_subgraphs(G, subgraphs, k, lambda_param, min_subset_size, max_subset_size, k_hop, path)
-    
+    plot_save_graph(
+        G,
+        k,
+        lambda_param,
+        min_subset_size,
+        max_subset_size,
+        k_hop,
+        path,
+        title="Original Graph",
+    )
+    plot_save_subgraphs(
+        G, subgraphs, k, lambda_param, min_subset_size, max_subset_size, k_hop, path
+    )
+
     # Plot the hypergraph
-    plot_save_hypergraph(hyper_dic, k, lambda_param, min_subset_size, max_subset_size, k_hop, path)
+    plot_save_hypergraph(
+        hyper_dic, k, lambda_param, min_subset_size, max_subset_size, k_hop, path
+    )
